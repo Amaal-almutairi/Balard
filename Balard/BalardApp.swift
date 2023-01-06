0505ed3071e0dc76db19dbfcf67a413e7caa2509
amaal <amaal772@gmail.com>
2023-01-06 19:28:03 +0300

    Initial Commit

---
+++ Balard/BalardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalardApp.swift
+//  Balard
+//
+//  Created by Amaal Almutairi on 13/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct BalardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
