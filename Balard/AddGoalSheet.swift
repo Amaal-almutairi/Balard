36dfaa2ba9b56b79c36ca67a313ae5283d08b618
amaal <amaal772@gmail.com>
2023-01-22 15:43:16 +0300

    remove comment

--- Balard/AddGoalSheet.swift
+++ Balard/AddGoalSheet.swift
@@ -30,58 +30,53 @@ struct AddGoalSheet: View {
                 } .padding()
                 VStack(alignment: .leading){
                     Text("Text5")
-                    // Text("You need to fill your Target goal !")
                     
                     TextField("Text6", text: $numberOnly.goalBalancevalue).alert(isPresented: $showAddCardAlert, content: {
                         Alert(title:Text("You need to fill your Target goal !"))
                     })
                     
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                       // .placeholder(when: numberOnly.goalBalancevalue.isEmpty) {
-//                    Text("Target Goal")
-//                   .foregroundColor(.blue)
-//                   .bold()
-                 //   }
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
                 }.padding()
-                  
-                        VStack(alignment: .leading){
-                            Text("Text7")
-                            TextField("Text9", text: $numberOnly.monthsvalue)
-                                .textFieldStyle(RoundedBorderTextFieldStyle())
-                            
-                        }.padding()
+                
+                VStack(alignment: .leading){
+                    Text("Text7")
+                    TextField("Text9", text: $numberOnly.monthsvalue)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                }.padding()
                 VStack(alignment: .center){
                     
                     Button("Text21", action: {
                         if validateTex(){
-                         
-                                let goalAmount: Int = Int(numberOnly.goalBalancevalue) ?? 0
-                                let monthInt: Int = Int(numberOnly.monthsvalue) ?? 2
-                          
-                                goalVM.addCardGoal(goalName: name, goalBalance:  goalAmount, months:monthInt)
                             
-                                name = ""
-                                numberOnly.monthsvalue = ""
-                                numberOnly.goalBalancevalue = ""
-                                prezsentationMode.wrappedValue.dismiss()
+                            let goalAmount: Int = Int(numberOnly.goalBalancevalue) ?? 0
+                            let monthInt: Int = Int(numberOnly.monthsvalue) ?? 2
+                            
+                            goalVM.addCardGoal(goalName: name, goalBalance:  goalAmount, months:monthInt)
+                            
+                            name = ""
+                            numberOnly.monthsvalue = ""
+                            numberOnly.goalBalancevalue = ""
+                            prezsentationMode.wrappedValue.dismiss()
                         }else{
                             showAddCardAlert = true
                             
                         }
-                       
+                        
                     }).alert("You need to fill your Target goal !", isPresented: $showAddCardAlert){
                         Button("OK", role: .cancel) { }
                     }
-                  //  .disabled(numberOnly.goalBalancevalue.isEmpty)
+                    //  .disabled(numberOnly.goalBalancevalue.isEmpty)
                     .modifier(Items.ButtonModifier()) }
-              
-                   
-
-                }
-        
+                
+                
+                
             }
             
         }
+        
+    }
     func validateTex() -> Bool{
         
         if !numberOnly.goalBalancevalue.isEmpty {
@@ -89,9 +84,9 @@ struct AddGoalSheet: View {
         }
         return false
     }
-        
-        
-    }
+    
+    
+}
 
 
 
@@ -103,40 +98,4 @@ struct AddGoalSheet_Previews: PreviewProvider {
     }
 }
 
-extension View {
-    func placeholder<Content: View> ( when shouldShow: Bool,
-   alignment: Alignment = .leading,
-                                      
- @ViewBuilder placeholder: () -> Content) -> some View {
-   ZStack(alignment: alignment) {
-     placeholder().opacity(shouldShow ? 1 : 0)
-         self
-        }
-    }
-}
 
-/*
- .alert("List Name", isPresented: $showAddCardAlert, actions: {
-     Button("Text21", action: {
-         let goalAmount: Int = Int(numberOnly.goalBalancevalue) ?? 0
-         let monthInt: Int = Int(numberOnly.monthsvalue) ?? 2
-         
-         if !goalBalance.isEmpty {
-             goalVM.addCardGoal(goalName: name, goalBalance:  goalAmount, months:monthInt)
-             name = ""
-             numberOnly.monthsvalue = ""
-             numberOnly.goalBalancevalue = ""
-             prezsentationMode.wrappedValue.dismiss()
-             
-         }
-     }).modifier(Items.ButtonModifier())
-     Button("Cancel", role: .cancel, action: {})
- } , message: {
-     Text("Please enter the name of list.")
- })
- 
- //                        Text(" \(goalBalance.isEmpty ? "You need to fill your Target goal !" : "" )")
- ////                        Text("\("You need to fill your Target goal !")")
- ////                            .foregroundColor(Color.red).font(.caption)
- //                    }
- */
